ce78ba5c3e8e94b115b6e298debfd355335fa44d
GounKim <kgukgu2001@gmail.com>
2022-02-28 18:58:15 +0900

    add 회원 등록 DB연동

--- ShopMiniMall/src/com/service/member/MemberService.java
+++ ShopMiniMall/src/com/service/member/MemberService.java
@@ -1,6 +1,9 @@
 package com.service.member;
 
+import com.dto.member.MemberDTO;
+
 public interface MemberService {
 	
 	public int idDuplicateCheck(String userid) throws Exception;
+	public int memberAdd(MemberDTO dto) throws Exception;
 }
--- ShopMiniMall/src/com/service/member/MemberServiceImpl.java
+++ ShopMiniMall/src/com/service/member/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import com.config.MySqlSessionFactory;
 import com.dao.member.MemberDAO;
+import com.dto.member.MemberDTO;
 
 public class MemberServiceImpl implements MemberService {
 
@@ -18,7 +19,21 @@ public class MemberServiceImpl implements MemberService {
 		} finally {
 			session.close();
 		}
+		return num;
+	}
+
+	@Override
+	public int memberAdd(MemberDTO dto) throws Exception {
+		int num = 0;
 		
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			MemberDAO dao = new MemberDAO();
+			num = dao.memberAdd(session, dto);
+			session.commit();
+		} finally {
+			session.close();
+		}
 		return num;
 	}
 
